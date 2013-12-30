0418f15083e8e60cecee04898436a8957ca85dc1
Mike Bayer <mike_mp@zzzcomputing.com>
2013-12-29 21:58:16 -0500

    dev

--- sphinx_paramlinks/sphinx_paramlinks.py
+++ sphinx_paramlinks/sphinx_paramlinks.py
@@ -1,3 +1,4 @@
+#!coding: utf-8
 import re
 from docutils import nodes
 from docutils.transforms import Transform
@@ -9,9 +10,8 @@ def autodoc_process_docstring(app, what, name, obj, options, lines):
 
     def _cvt_param(name, line):
         def cvt(m):
-            txt = ":param %s_autodoc_link_param_%s.%s:" % (
+            return ":param %s_sphinx_paramlinks_%s.%s:" % (
                             m.group(1) or '', name, m.group(2))
-            return txt
         return re.sub(r'^:param ([^:]+? )?([^:]+?):', cvt, line)
 
     if what in ('function', 'method', 'class'):
@@ -25,11 +25,11 @@ class LinkParams(Transform):
 
         for ref in self.document.traverse(nodes.strong):
             text = ref.astext()
-            if text.startswith("_autodoc_link_param_"):
-                components = re.match(r'_autodoc_link_param_(.+)\.(.+)$', text)
+            if text.startswith("_sphinx_paramlinks_"):
+                components = re.match(r'_sphinx_paramlinks_(.+)\.(.+)$', text)
                 location, paramname = components.group(1, 2)
 
-                refid = "%s.%s" % (location, paramname)
+                refid = "%s.params.%s" % (location, paramname)
                 ref.parent.insert(0,
                     nodes.target('', '', ids=[refid])
                 )
@@ -47,6 +47,8 @@ class LinkParams(Transform):
 
 
 def make_param_ref(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    import pdb
+    pdb.set_trace()
     prefix = "#%s"
     ref = "foo"
     node = nodes.reference(rawtext, prefix % text, refuri=ref, **options)
@@ -54,19 +56,18 @@ def make_param_ref(name, rawtext, text, lineno, inliner, options={}, content=[])
 
 
 def add_stylesheet(app):
-    app.add_stylesheet('autodoc_links.css')
+    app.add_stylesheet('sphinx_paramlinks.css')
 
 def copy_stylesheet(app, exception):
     if app.builder.name != 'html' or exception:
         return
-    app.info(bold('Copying autodoc_links stylesheet... '), nonl=True)
-    dest = os.path.join(app.builder.outdir, '_static', 'autodoc_links.css')
+    app.info(bold('Copying sphinx_paramlinks stylesheet... '), nonl=True)
+    dest = os.path.join(app.builder.outdir, '_static', 'sphinx_paramlinks.css')
     source = os.path.abspath(os.path.dirname(__file__))
-    copyfile(os.path.join(source, "autodoc_links.css"), dest)
+    copyfile(os.path.join(source, "sphinx_paramlinks.css"), dest)
     app.info('done')
 
 def setup(app):
-    app.add_config_value("autodoc_links_convert_modname", {}, 'env')
     app.add_transform(LinkParams)
     app.connect('autodoc-process-docstring', autodoc_process_docstring)
     app.add_role('paramref', make_param_ref)
