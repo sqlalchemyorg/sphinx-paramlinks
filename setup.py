bf6b94b756493f6039d9da3dce8bca666efc1434
Mike Bayer <mike_mp@zzzcomputing.com>
2021-01-29 13:39:13 -0500

    Repair version token reg for Black formatting
    We should move this to setup.cfg style but for now
    just get things to work.

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ from setuptools import setup
 v = open(
     os.path.join(os.path.dirname(__file__), "sphinx_paramlinks/__init__.py")
 )
-VERSION = re.compile(r".*__version__ = '(.*?)'", re.S).match(v.read()).group(1)
+VERSION = (
+    re.compile(r".*__version__ = [\"'](.*?)[\"']", re.S)
+    .match(v.read())
+    .group(1)
+)
 v.close()
 
 readme = os.path.join(os.path.dirname(__file__), "README.rst")
